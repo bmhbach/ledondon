86c26b68145eead270deb7c7f56654f24917764e
Benjamin Bach <bmhbach@gmail.com>
2020-11-21 22:32:43 -0800

    Update animation and styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ const App = () => {
 		<div className="App">
 			<header className="App-header">
 				<p>ledondon</p>
-				<Canvas>
-					<ambientLight intensity={0.5} />
-					<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-					<pointLight position={[-10, -10, -10]} />
-					<Box position={[0, 0, 0]} />
-				</Canvas>
 			</header>
+			<Canvas className="Canvas">
+				<ambientLight intensity={0.5} />
+				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+				<pointLight position={[-10, -10, -10]} />
+				<Box position={[0, 0, 0]} />
+			</Canvas>
 		</div>
 	);
 };
--- src/draw/Box.tsx
+++ src/draw/Box.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import { MeshProps, useFrame } from "react-three-fiber";
-import { Mesh } from "three";
+import { Mesh, Vector3 } from "three";
 
 const Box = (props: MeshProps) => {
 	// This reference will give us direct access to the mesh
@@ -9,15 +9,21 @@ const Box = (props: MeshProps) => {
 	// Set up state for the hovered and active state
 	const [hovered, setHover] = useState(false);
 	const [active, setActive] = useState(false);
+	const [sizes, setSizes] = useState(new Vector3(1,1,1));
 	const [time, setTime] = useState(0);
 
 	// Rotate mesh every frame, this is outside of React without overhead
 	useFrame(() => {
-		if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.x += 0.03;
-		if (mesh.current) mesh.current.rotation.y = mesh.current.rotation.y += 0.03;
+		if (mesh.current)
+			mesh.current.rotation.x = mesh.current.rotation.x += 0.03;
+		if (mesh.current)
+			mesh.current.rotation.y = mesh.current.rotation.y += 0.03;
 
-		if (mesh.current) 
-			mesh.current.position.x += Math.sin(Math.PI * 2 * time / 600 + (Math.PI / 2)) * 0.05;
+		if (mesh.current)
+			mesh.current.position.x += Math.sin(Math.PI * 2 * time / 600 + (Math.PI / 2)) * 0.035;
+		if (mesh.current)
+			mesh.current.position.y += Math.sin(Math.PI * 2 * time / 300 + (Math.PI / 2)) * 0.035;
+
 		setTime(time + 1);
 	});
 
@@ -25,12 +31,16 @@ const Box = (props: MeshProps) => {
 		<mesh
 			{...props}
 			ref={mesh}
-			scale={active ? [2.5, 2.5, 2.5] : [1, 1, 1]}
-			onClick={(event) => setActive(!active)}
+			scale={active ? sizes : [1, 1, 1]}
+			onClick={(event) => {
+				if (!active)
+					setSizes(new Vector3(Math.random() * 2.5, Math.random() * 2.5, Math.random() * 2.5));
+				setActive(!active);
+			}}
 			onPointerOver={(event) => setHover(true)}
 			onPointerOut={(event) => setHover(false)}>
 			<boxBufferGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+			<meshStandardMaterial color={hovered ? "lightyellow" : "lightblue"} />
 		</mesh>
 	);
 };
